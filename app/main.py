85bca1013020fab035c2f25b77e05bb64cac92f2
Nathan <nathan.sujatno@gmail.com>
2025-11-18 14:41:40 -0600

    modified resume uploader endpoint to just get the full text

--- app/main.py
+++ app/main.py
@@ -1,11 +1,10 @@
 from fastapi import FastAPI
 from app.config import settings
-from app.routes import resume, jobs
+from app.routes import resume
 
 app = FastAPI()
 
 app.include_router(resume.router, prefix="/api")
-app.include_router(jobs.router, prefix="/api")
 
 @app.get("/api/health")
 def health_check():
--- app/routes/resume.py
+++ app/routes/resume.py
@@ -1,92 +1,27 @@
-import uuid
-from fastapi import APIRouter, HTTPException, status
-from openai import OpenAI
-from app.config import settings, chroma_client
-from app.models import ResumeUploadRequest
+from fastapi import APIRouter, UploadFile, File, HTTPException, status
+import pdfplumber
+import io
 
 router = APIRouter()
-client = OpenAI(api_key=settings.openai_api_key)
 
-@router.post("/resume")
-async def upload_resume(request: ResumeUploadRequest):
-    resume_id = str(uuid.uuid4())
+@router.post("/upload-resume")
+async def upload_resume(file: UploadFile = File(...)) -> str:
+    # validate file
+    if not file.filename.endswith(".pdf"):
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Only pdf files are allowed")
 
-    chunks = []
-    chunk_metadata = []
-    chunk_ids = []
-
-    chunks.append(request.resume.text)
-    chunk_metadata.append({
-        "resume_id": resume_id,
-        "chunk_type": "full_text",
-        "roles": ",".join(request.preferences.role),
-        "experience_level": request.preferences.experience_level
-    })
-    chunk_ids.append(f"{resume_id}_full")
-
-    # sills as separate chunk
-    if request.resume.skills:
-        skills_text = "Skills: " + ", ".join(request.resume.skills)
-        chunks.append(skills_text)
-        chunk_metadata.append({
-            "resume_id": resume_id,
-            "chunk_type": "skills",
-            "roles": ",".join(request.preferences.role),
-            "experience_level": request.preferences.experience_level
-        })
-        chunk_ids.append(f"{resume_id}_skills")
-
-    # each experience as a separate chunk
-    if request.resume.experience:
-        for i, exp in enumerate(request.resume.experience):
-            chunks.append(f"Experience: {exp}")
-            chunk_metadata.append({
-                "resume_id": resume_id,
-                "chunk_type": "experience",
-                "experience_index": i,
-                "roles": ",".join(request.preferences.role),
-                "experience_level": request.preferences.experience_level
-            })
-            chunk_ids.append(f"{resume_id}_exp_{i}")
-            
-    # each project as a separate chunk
-    if request.resume.projects:
-        for i, proj in enumerate(request.resume.projects):
-            chunks.append(f"Project: {proj}")
-            chunk_metadata.append({
-                "resume_id": resume_id,
-                "chunk_type": "project",
-                "project_index": i,
-                "roles": ",".join(request.preferences.role),
-                "experience_level": request.preferences.experience_level
-            })
-            chunk_ids.append(f"{resume_id}_proj_{i}")
-    
     try:
-        response = client.embeddings.create(
-            model="text-embedding-3-small",
-            input=chunks
-        )
-
+        file_content = await file.read()
+        response = read_pdf_plumber(file_content)
+        return response
     except Exception as e:
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"Error creating embeddings for resume {str(e)}")
-    
-    try:
-        collection = chroma_client.get_or_create_collection(name="resumes")
-        embeddings = [item.embedding for item in response.data]
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"Error while reading pdf: {str(e)}")
 
-        # Store all chunks in ChromaDB
-        collection.add(
-            embeddings=embeddings,
-            documents=chunks,
-            ids=chunk_ids,
-            metadatas=chunk_metadata
-        )
-    except Exception as e:
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"Error storing resume embeddings into db {str(e)}")
-    
-    
-    return {
-        "resume_id": resume_id,
-        "chunks_stored": len(chunks)
-    }
+def read_pdf_plumber(file_bytes):
+    text = ""
+    with pdfplumber.open(io.BytesIO(file_bytes)) as pdf:
+        for page in pdf.pages:
+            page_text = page.extract_text()
+            if page_text:
+                text += page_text + "\n"
+    return text
